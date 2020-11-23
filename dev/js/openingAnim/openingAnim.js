e305dacca3f8b840dd90f15e306d1ffa0dbe9d81
K-Hendry <63383187+K-Hendry@users.noreply.github.com>
2020-11-23 10:57:27 -0500

    Opening sequence up to logo reveal

--- dev/js/openingAnim/openingAnim.js
+++ dev/js/openingAnim/openingAnim.js
@@ -1,12 +1,24 @@
 import {gsap} from "gsap";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+
+gsap.registerPlugin(MorphSVGPlugin);
 
 const openingLinesTL = gsap.timeline();
 
 
 export function openingLinesAnimation(){
     
-    openingLinesTL.from("#intro-line", {transformOrigin: "center", scaleX: 0, duration:.5});
+    openingLinesTL.from("#line-base", {transformOrigin: "center", scaleX: 0, duration:.75})
+    .to("#line-base",{duration:.5, morphSVG:"#mountains-line"}, "+=0")
+    .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
+    .to("#line-base",{duration:.5, morphSVG:"#dunes-line"}, "+=0")
+    .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
+    .to("#line-base",{duration:.5, morphSVG:"#waves-line"}, "+=0")
+    .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
+    .to("#line-base",{duration:.5, morphSVG:"#city-lines"}, "+=0")
+    .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
+    .to("#line-base",{transformOrigin: "center", scaleX: 0, duration:.75})
+    .from("#jeep-logo", {transformOrigin: "center", scaleX: 0, duration:.75});
 
     return openingLinesTL;
-
 }
