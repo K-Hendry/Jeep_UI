7eb3867efcfaffa9fdeb4dc313a5c7419e4e8efd
K-Hendry <63383187+K-Hendry@users.noreply.github.com>
2020-11-30 08:07:48 -0500

    Modified Opening INtro

--- dev/js/openingAnim/openingAnim.js
+++ dev/js/openingAnim/openingAnim.js
@@ -11,7 +11,7 @@ const openingLinesTL = gsap.timeline();
 export function openingLinesAnimation(){
     
     openingLinesTL.from("#line-base", {transformOrigin: "center", scaleX: 0, duration:.75})
-    .to("#line-base",{duration:.5, morphSVG:"#mountains-line"}, "+=0")
+    .to("#line-base",{transformOrigin: "center", duration:.5, morphSVG:"#mountains-line"}, "overlap")
     .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
     .to("#line-base",{duration:.5, morphSVG:"#dunes-line"}, "+=0")
     .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
@@ -20,9 +20,9 @@ export function openingLinesAnimation(){
     .to("#line-base",{duration:.5, morphSVG:"#city-lines"}, "+=0")
     .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
     .to("#line-base",{transformOrigin: "center", scaleX: 0, duration:.5})
-    .to("#logo",{transformOrigin: "center", x: -500 , y: -500, scaleX: 100, duration:.75});
-    //.from("#dash-outline",{duration:.5, drawSVG:"0%"})
-    //.to("#logo",{xPercent:"-50%", yPercent: "-50%"});
+    .from("#jeep-logo",{transformOrigin: "center", duration:.75, scaleX:0})
+    //.from("#dash-outline",{duration:.5, drawSVG:"10%"})
+    .to("#jeep-logo",{xPercent:"-100", yPercent: "-100", visibility: "display"});
 
     return openingLinesTL;
 }
