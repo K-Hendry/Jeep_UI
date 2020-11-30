dbc2087e0958efc0bac4b8db20519136ec9db810
K-Hendry <63383187+K-Hendry@users.noreply.github.com>
2020-11-30 11:17:38 -0500

    Dash Draw SVG

--- dev/js/openingAnim/dashOutline.js
+++ dev/js/openingAnim/dashOutline.js
@@ -8,17 +8,7 @@ const dashOutlineTL = gsap.timeline();
 
 export function dashOutlineAnimation(){
     
-    //dashOutlineTL.from("#line-base", {transformOrigin: "center", scaleX: 0, duration:.75})
-    // .to("#line-base",{duration:.5, morphSVG:"#mountains-line"}, "+=0")
-    // .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
-    // .to("#line-base",{duration:.5, morphSVG:"#dunes-line"}, "+=0")
-    // .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
-    // .to("#line-base",{duration:.5, morphSVG:"#waves-line"}, "+=0")
-    // .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
-    // .to("#line-base",{duration:.5, morphSVG:"#city-lines"}, "+=0")
-    // .to("#line-base",{duration:.5, morphSVG:"#line-base"}, "+=0")
-    // .to("#line-base",{transformOrigin: "center", scaleX: 0, duration:.75})
-    // .from("#jeep-logo", {transformOrigin: "center", scaleX: 0, duration:.75});
+    dashOutlineTL.from("#dash-outline", {duration:1, drawSVG: 0})
 
     return dashOutlineTL;
 }
