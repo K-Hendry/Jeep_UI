49f6c4ebea1c683c33d42255147617b7cb1f2573
K-Hendry <63383187+K-Hendry@users.noreply.github.com>
2020-12-10 10:25:29 -0500

    risky business pays

--- dev/js/openingAnim/dashActivity.js
+++ dev/js/openingAnim/dashActivity.js
@@ -5,9 +5,13 @@ const dashActivityTL = gsap.timeline();
 
 export function dashActivityAnimation(){
     
-    dashActivityTL.to("#speed-needle", {transformOrigin: "center", rotate: 45})
-    .to("#rpm-needle", {transformOrigin: "center", rotate: 50})
-    .to("#arrow", {yPercent: -50, duration: 3});
+    dashActivityTL.to("#speed-needle", {transformOrigin: "right top", rotate: 45}, "+=.75")
+    .to("#rpm-needle", {transformOrigin: "right top", rotate: 50})
+    .to("#arrow", {yPercent: -150, duration: 3}, "-=.95")
+    .to("#speed-needle", {transformOrigin: "right top", rotate: 10}, "+=.75")
+    .to("#rpm-needle", {transformOrigin: "right top", rotate: 40})
+    .to("#arrow", {yPercent: -150, duration: 3}, "-=.95")
+    ;
     
 
     return dashActivityTL;
--- dev/js/openingAnim/dashOutline.js
+++ dev/js/openingAnim/dashOutline.js
@@ -14,18 +14,25 @@ export function dashOutlineAnimation(){
     .from("#speedometer-rpm", {duration: .25, alpha: 0})
     .from("#map-lines", {duration: .5, alpha: 0})
     .from("#arrow", {duration: .25, alpha: 0})
-    .from("#NE", {duration: .25, alpha: 0})
     .from("#MODE", {duration: .25, alpha: 0})
-    .from("#city", {duration: .25, alpha: 0})
+    .from("#city", {duration: .25, alpha: 0}, "+=.25")
     .to("#city",{duration:.35, morphSVG:"#mountains"}, "+=0")
+    .to("#MODE",{duration:.05, alpha: 0}, "-=.35")
+    .from("#STEEP",{duration:.05, alpha: 0}, "-=.35")
     .to("#city",{duration:.35, morphSVG:"#waves"}, "+=0")
+    .to("#STEEP",{duration:.05, alpha: 0}, "-=.35")
+    .from("#SURF",{duration: 0, alpha: 0}, "-=.35")
     .to("#city",{duration:.35, morphSVG:"#dunes"}, "+=0")
+    .to("#SURF",{duration:.05, alpha: 0}, "-=.35")
+    .from("#SAND",{duration: 0, alpha: 0}, "-=.35")
     .to("#city",{duration:.35, morphSVG:"#city"}, "+=0")
+    .to("#SAND",{duration:.05, alpha: 0}, "-=.35")
+    .from("#CITY-WORD",{duration: 0, alpha: 0}, "-=.35")
     .from("#gas-outline2", {duration: .25, alpha: 0})
     .from("#gas-cap", {duration: .25, alpha: 0})
     .from("#G", {duration: .25, alpha: 0})
-    .from("#liquid", {duration: 1.75, alpha: 0, yPercent: 150, xPercent: -550, delay:0})
-    .from("#jeep-top", {duration: .5, alpha: 0});
+    .from("#liquid", {duration: .5, alpha: 0, yPercent: 550, xPercent: -1750, delay:0})
+    .from("#jeep-top", {duration: .5, alpha: 0}, "+=.75");
 
     return dashOutlineTL;
 }
